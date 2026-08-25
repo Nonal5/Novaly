93ee636eacc36e76daf87900c2b608c59a938c9a
Noah Valentin <noah@MacBook-Air-de-Noah.local>
2026-08-25 18:50:34 +0200

    fix: Suppression du plugin shell en trop et compilation propre

--- novaly-launcher/src-tauri/src/lib.rs
+++ novaly-launcher/src-tauri/src/lib.rs
@@ -7,10 +7,9 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         // 🚀 L'UPDATER EST INITIALISÉ JUSTE ICI :
         .plugin(tauri_plugin_updater::Builder::new().build()) 
-        .invoke_handler(tauri::generate_handler![greet]) // (Si tu as la fonction greet)
+        // Laisse la ligne invoke_handler si tu l'avais avant
         .run(tauri::generate_context!())
         .expect("erreur lors du lancement de l'application tauri");
 }
