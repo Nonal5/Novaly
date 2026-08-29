b3677d72723b823774dc5085041d86e872374733
Noah Valentin <noah@device-24.home>
2026-08-30 00:18:48 +0200

    Mise à jour globale v1.0.61

--- novaly-launcher/src-tauri/src/lib.rs
+++ novaly-launcher/src-tauri/src/lib.rs
@@ -1,8 +1,9 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         // 🚀 L'UPDATER EST INITIALISÉ JUSTE ICI :
-        .plugin(tauri_plugin_updater::Builder::new().build()) 
+        .plugin(tauri_plugin_updater::Builder::new().build())
         // Laisse la ligne invoke_handler si tu l'avais avant
         .run(tauri::generate_context!())
         .expect("erreur lors du lancement de l'application tauri");
